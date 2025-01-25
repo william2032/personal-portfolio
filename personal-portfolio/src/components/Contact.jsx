8faaea7c29a57d169e47a9bc8d48a7d1218876bc
william2032 <kivindukilonzo@gmail.com>
2025-01-24 23:21:10 -0800

    i will add projects i've compeleted later

---
+++ personal-portfolio/src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div className='max-w-screen-xl mx-auto contact'>
+            <div>
+                <h1>Get in touch</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Contact 
